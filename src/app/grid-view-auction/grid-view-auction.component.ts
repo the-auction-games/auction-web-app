2684ec2c57701df95550664eb63aa7daa82302c5
Joel Smith <joelsmith.2019@gmail.com>
2023-02-25 21:44:06 -0700

    Update auction grid view

--- src/app/grid-view-auction/grid-view-auction.component.ts
+++ src/app/grid-view-auction/grid-view-auction.component.ts
@@ -16,6 +16,12 @@ export class GridViewAuctionComponent {
   // The auction
   @Input() auction: Auction | undefined;
 
+  // Is the auction a demo
+  @Input() isDemo: boolean = false;
+
+  // The current bid
+  protected currentBid: number = 0;
+
   // The auction refresh interval
   private auctionRefreshInterval: NodeJS.Timer | undefined;
 
@@ -29,12 +35,20 @@ export class GridViewAuctionComponent {
   constructor(
     private router: Router,
     private sync: AuctionSyncService
-  ) {}
+  ) { }
 
   // Method called when the component is initialized
   ngOnInit(): void {
+    // Get the current bid
+    this.currentBid = AuctionUtils.getCurrentBid(this.auction);
+
     // Refresh the auction every 5 seconds
-    this.countdownInterval = this.sync.updateAuction(this.auction, 5000);
+    if (!this.isDemo) {
+      this.auctionRefreshInterval = this.sync.updateAuction(this.auction, 5000, (auction) => {
+        // Update current bid
+        this.currentBid = AuctionUtils.getCurrentBid(auction);
+      });
+    }
 
     // Get the formatted expiration
     this.expiration = AuctionUtils.getFormattedExpiration(this.auction);
@@ -48,17 +62,24 @@ export class GridViewAuctionComponent {
   // Method called when the component is destroyed
   ngOnDestroy(): void {
     // Clear intervals
-    clearInterval(this.auctionRefreshInterval);
+    if (!this.isDemo) clearInterval(this.auctionRefreshInterval);
     clearInterval(this.countdownInterval);
   }
 
   // Method called when the user clicks on the auction
   public onView(): void {
-    
-    // Log the auction id
-    console.log('Viewing auction: ' + this.auction?.id);
 
-    // Navigate to the market page with the auction id as a parameter
-    this.router.navigate(['/market', this.auction?.id]);
+    // Check if a demo
+    if (this.isDemo) {
+      // Navigate to the market
+      this.router.navigate(['/market']);
+
+    } else {
+      // Log the auction id
+      console.log('Viewing auction: ' + this.auction?.id);
+
+      // Navigate to the market page with the auction id as a parameter
+      this.router.navigate(['/market', this.auction?.id]);
+    }
   }
 }
